085d93bd235122f66722ae3a7da1503c3b90451f
Anibal Alfredo De la Vega <adelavega@cdainfo.com>
2021-11-11 10:56:47 -0300

    Se inicia y agrega proyecto Maven Java.

---
+++ src/main/java/BDD_Framework/CucumberBDD/App.java
@@ -0,0 +1,13 @@
+package BDD_Framework.CucumberBDD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
