4171fcd3f6c17c2d2aae6400cee180a97d77bf54
Anibal Alfredo De la Vega <adelavega@cdainfo.com>
2021-11-12 12:11:34 -0300

    Agregado de lector de archivo JSON y tipos de datos.

--- src/main/java/dataProvider/JSonDataReader.java
+++ src/main/java/dataProvider/JSonDataReader.java
@@ -26,31 +26,31 @@ public class JSonDataReader {
 		Gson gson = new Gson();
 		BufferedReader bufferReader = null;
 		try {
-			bufferReader = new BufferedReader ( new FileReader ( customerPath ));
+			bufferReader = new BufferedReader(new FileReader(customerPath));
 			CustomerDataType[] customer = gson.fromJson(bufferReader, CustomerDataType[].class);
 			return Arrays.asList(customer);
-		} catch ( FileNotFoundException o) {
-			throw new RuntimeException ( "JSON file not found at path " + customerPath);
+		} catch (FileNotFoundException o) {
+			throw new RuntimeException("JSON file not found at path " + customerPath);
 		} finally {
 			try { 
-				if ( bufferReader != null ) 
+				if(bufferReader != null) 
 					bufferReader.close();
-			} catch ( IOException ignore ) {	}
+			} catch (IOException ignore) {	}
 		}
 	}
 	
-	public final CustomerDataType getCustomerByName( String customerName ) {
-		return customerList.stream().filter(x -> x.firstName.equalsIgnoreCase( customerName )).findAny().get();
+	public final CustomerDataType getCustomerByName(String customerName) {
+		return customerList.stream().filter(x -> x.firstName.equalsIgnoreCase(customerName)).findAny().get();
 	}
 	
-	public final CustomerDataType getCredicartByName( String customerName ) {
-		for ( CustomerDataType customer : customerList ) {
-			if ( customer.firstName.equalsIgnoreCase( customerName )) return customer;
+	public final CustomerDataType getCredicartByName(String customerName) {
+		for (CustomerDataType customer : customerList) {
+			if (customer.firstName.equalsIgnoreCase(customerName)) return customer;
 		}
 		return null;
 	}
 	
-	public final CustomerDataType getDataByID ( Integer customerID ) {
+	public final CustomerDataType getDataByID(Integer customerID) {
 		// return customerList.stream().filter(x->x.ID.equalsIgnoreCase(customerID)).findAny().get();
 		return customerList.stream().filter(x->x.ID.equals(customerID)).findAny().get();
 	}
